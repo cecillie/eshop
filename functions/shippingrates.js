df90ea2314b1cf5030c83fe8f6c4e46d776badcc
Arnaud Ligny <arnaud@ligny.org>
2021-01-29 19:37:22 +0100

    Update shippingrates.js

--- functions/shippingrates.js
+++ functions/shippingrates.js
@@ -4,14 +4,19 @@ exports.handler = async ({ body, headers }) => {
 
     // default rates
     rates = [{
-      cost: 2,
-      description: 'Livraison à vélo (Montreuil)',
-      userDefinedId: 'shipping_20',
-    }, {
       cost: 0,
       description: 'Retrait (Paris 20ème)',
       userDefinedId: 'shipping_30'
     }];
+
+    // Montreuil
+    if (obj['content']['shippingAddressPostalCode'] == '93100') {
+      rate_tube = [{
+        cost: 2,
+        description: 'Livraison à vélo (Montreuil)',
+        userDefinedId: 'shipping_20',
+      }];
+    }
 
     // A3
     rate_tube = [{

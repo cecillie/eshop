d0ad59523728bfe7265b29394c99eb54ca42b820
Arnaud Ligny <arnaud@ligny.org>
2021-01-29 19:11:09 +0100

    Update shippingrates.js

--- functions/shippingrates.js
+++ functions/shippingrates.js
@@ -4,30 +4,36 @@ exports.handler = async ({ body, headers }) => {
 
     return {
       statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         errors: [{
-          key: "invalid_postal_code",
-          message: "The postal code is invalid."
+          key: 'invalid_postal_code',
+          message: 'The postal code is invalid.'
         }]
       })
     };
 
     return {
       statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         rates: [{
           cost: 10,
-          description: "10$ shipping",
-          userDefinedId: "shipping_10",
+          description: '10$ shipping',
+          userDefinedId: 'shipping_10',
         }, {
           cost: 20,
-          description: "20$ shipping",
+          description: '20$ shipping',
           guaranteedDaysToDelivery: 5,
-          userDefinedId: "shipping_20",
+          userDefinedId: 'shipping_20',
         }, {
           cost: 30,
-          description: "30$ shipping",
-          userDefinedId: "shipping_30"
+          description: '30$ shipping',
+          userDefinedId: 'shipping_30'
         }]
       })
     };

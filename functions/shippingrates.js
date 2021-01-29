ca54e9007cdb9fe0887252e2d4eec26b319f1477
Arnaud Ligny <arnaud@ligny.org>
2021-01-29 17:32:42 +0100

    Create shippingrates.js

---
+++ functions/shippingrates.js
@@ -0,0 +1,15 @@
+exports.handler = async ({ body, headers }) => {
+  try {
+    const obj = JSON.parse(body);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ received: true }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: `Webhook Error: ${err.message}`,
+    };
+  }
+};

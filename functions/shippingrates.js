5ee202e5c5356197d0853594b2437b102220c707
Arnaud Ligny <arnaud@ligny.org>
2021-01-29 20:04:58 +0100

    Update shippingrates.js

--- functions/shippingrates.js
+++ functions/shippingrates.js
@@ -2,42 +2,40 @@ exports.handler = async ({ body, headers }) => {
   try {
     const obj = JSON.parse(body);
 
-    let found = obj.find(e => e.value === 'A3');
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(found)
-    };
-
-    // default rates
+    // Retrait dans le 20ème
     rates = [{
       cost: 0,
       description: 'Retrait (Paris 20ème)',
       userDefinedId: 'retrait'
     }];
-    // Montreuil
+    // Livraison à vélo
     rate_montreuil = {
       cost: 2,
       description: 'Livraison à vélo (Montreuil)',
       userDefinedId: 'livraison_velo'
     };
-    // A3
+    // Tube
     rate_tube = {
       cost: 10,
       description: 'Colissimo (tube)',
       userDefinedId: 'colissimo_tube'
     };
-    // A5
+    // Enveloppe
     rate_enveloppe = {
       cost: 8,
       description: 'Colissimo (enveloppe)',
       userDefinedId: 'colissimo_enveloppe'
     };
 
+    // Montreuil
     if (obj.content.shippingAddressPostalCode == '93100') {
       rates.push(rate_montreuil);
+    }
+    // Format A3
+    for (var i = 0; i < obj.content.items.length; i++) {
+      if (obj.content.items[i].customFields[0].value == 'A3') {
+        rates.push(rate_tube);
+      }
     }
 
     return {

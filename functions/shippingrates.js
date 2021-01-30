208e37dd4ede4c4eb3235ffba2ec4e5011a4033b
Arnaud Ligny <arnaud@ligny.org>
2021-01-31 00:47:20 +0100

    Move rates in dedicated files

--- functions/shippingrates.js
+++ functions/shippingrates.js
@@ -1,57 +1,39 @@
+const livraison_velo = require('./data/rates/livraison_velo.json');
+const livraison_colissimo = require('./data/rates/livraison_colissimo.json');
+const livraison_lettre = require('./data/rates/livraison_lettre.json');
+const livraison_colissimo_lettre = require('./data/rates/livraison_colissimo_lettre.json');
+const retrait_paris_20 = require('./data/rates/retrait_paris_20.json');
+
+function createResponse(response) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(response)
+  };
+}
+
 exports.handler = async ({ body }) => {
   try {
     const obj = JSON.parse(body);
+    let rates = [];
 
     // Livraison FR uniquement
     if (obj.content.shippingAddressCountry != 'FR') {
-      return {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          errors: [{
-            key: 'error_country',
-            message: 'La livraison n\'est possible qu\'en France pour le moment.'
-          }]
-        })
-      };
+      return createResponse({
+        errors: [{
+          key: 'error_country',
+          message: 'La livraison n\'est possible qu\'en France pour le moment.'
+        }]
+      });
     }
 
-    // Retrait dans le 20ème
-    let rates = [{
-      cost: 0,
-      description: 'Retrait (Paris 20ème)',
-      userDefinedId: 'retrait'
-    }];
-    // Livraison à vélo
-    const rate_montreuil = {
-      cost: 0,
-      description: 'Livraison à vélo (Montreuil)',
-      userDefinedId: 'livraison_velo'
-    };
-    // Colissimo
-    const rate_colissimo = {
-      cost: 5,
-      description: 'Colissimo',
-      userDefinedId: 'livraison_colissimo'
-    };
-    // Lettre suivie
-    const rate_lettre = {
-      cost: 3,
-      description: 'Lettre suivie',
-      userDefinedId: 'livraison_lettre'
-    };
-    // Colissimo + Lettre suivie
-    const rate_2 = {
-      cost: 8,
-      description: 'Colissimo + Lettre suivie (envoi séparé)',
-      userDefinedId: 'livraison_colissimo_lettre'
-    };
-
+    // Retrait dans Paris 20ème
+    rates.push(retrait_paris_20);
     // Livraison à Montreuil
     if (obj.content.shippingAddressPostalCode == '93100') {
-      rates.push(rate_montreuil);
+      rates.push(livraison_velo);
     }
     // Quel(s) format(s)
     let a3 = 0; let a5 = 0;
@@ -67,36 +49,24 @@ exports.handler = async ({ body }) => {
         }
       }
     }
-    // Emballage selon le format
+    // Emballage(s) selon le(s) format(s)
     if (a3 >= 1 && a5 >= 1) {
-      rates.push(rate_2);
+      rates.push(livraison_colissimo_lettre);
     } else if (a3 >= 1 && a5 == 0) {
-      rates.push(rate_colissimo);
+      rates.push(livraison_colissimo);
     } else if (a5 >= 1 && a3 == 0) {
-      rates.push(rate_lettre);
+      rates.push(livraison_lettre);
     }
 
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        rates: rates
-      })
-    };
+    return createResponse({
+      rates: rates
+    });
   } catch (err) {
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        errors: [{
-          key: 'error',
-          message: 'Impossible d\'afficher les modes de livraison.'
-        }]
-      })
-    };
+    return createResponse({
+      errors: [{
+        key: 'error',
+        message: 'Impossible d\'afficher les modes de livraison.'
+      }]
+    });
   }
 };
